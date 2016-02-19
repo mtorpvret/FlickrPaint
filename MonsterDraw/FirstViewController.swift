6fed104970526c093419644593bd1fddb556c6a7
markus <markus@torpvret.se>
2016-02-19 20:34:03 +0100

    Initial Commit

---
+++ MonsterDraw/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MonsterDraw
+//
+//  Created by Markus Torpvret on 2016-02-19.
+//  Copyright © 2016 Markus Torpvret. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MonsterDraw/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MonsterDraw
+//
+//  Created by Markus Torpvret on 2016-02-19.
+//  Copyright © 2016 Markus Torpvret. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
